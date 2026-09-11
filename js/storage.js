f0fb6de244a3dd60c30ce31456ef465f21d31b03
Milen Milanov <milanovm229@gmail.com>
2026-09-12 00:35:39 +0300

    Add storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,1 +1,38 @@
+const STORAGE_KEY = "orbit-products";
 
+function loadProducts() {
+  const storedProducts = localStorage.getItem(STORAGE_KEY);
+
+  if (!storedProducts) {
+    localStorage.setItem(
+      STORAGE_KEY,
+      JSON.stringify(initialProducts)
+    );
+
+    return [...initialProducts];
+  }
+
+  try {
+    return JSON.parse(storedProducts);
+  } catch (error) {
+    console.error("Unable to load stored products:", error);
+
+    return [...initialProducts];
+  }
+}
+
+function saveProducts(products) {
+  localStorage.setItem(
+    STORAGE_KEY,
+    JSON.stringify(products)
+  );
+}
+
+function resetProducts() {
+  localStorage.setItem(
+    STORAGE_KEY,
+    JSON.stringify(initialProducts)
+  );
+
+  return [...initialProducts];
+}
